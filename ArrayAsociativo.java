785a0453b905bfb9430f0a64d6399e0bc4012c6b
JorgeL <jorgepalomares42@hotmail.com>
2017-05-18 17:47:40 +0200

    Inicio

---
+++ ArrayAsociativo.java
@@ -0,0 +1,75 @@
+
+
+public class ArrayAsociativo {
+	//Clases internas
+	private class Nodo{
+		private String clave;
+		private String valor;
+		private Nodo sig; //siguiente nodo
+		
+		public Nodo(String c, String v, Nodo s){
+			clave = c;
+			valor = v;
+			sig = s;
+		}
+	}
+	
+	//Variables de instancia
+	private Nodo primero;
+	
+	
+	//Constructores
+	public ArrayAsociativo(){
+		primero = null;
+	}
+	
+	public ArrayAsociativo(String[] claves, String[] valores){
+		this();
+		if (claves.length > 0){
+			primero = new Nodo(claves[0], valores[0],null);
+			for (int i = 1; i< claves.length; i++){
+				Nodo nuevo = new Nodo(claves[i], valores[i],primero);
+				primero = nuevo;
+			}
+		}
+	}
+	
+	//Métodos de instancia
+	
+	// Devuelve el número de entradas del arry asociativo.
+	public int size(){
+		return 0;
+		
+	}
+	
+	//Para una clave devuelve el valor asociado
+	public String get(String clave){
+		return clave;
+		
+	}
+	
+	//crea una  nueva entrada en la tabla en caso de existir la clave cambia al nuevo valor
+	public void put(String clave, String valor){
+		
+	}
+	
+	/*para una clave dada devuelve el valor asociado en caso de 
+	no haber devuelve valor por defecto*/
+	public String getOrElse(String clave, String valorPorDefecto){
+		return valorPorDefecto;
+		
+	}
+	
+	//Devuelve true si existe la clave o false si no
+	public boolean containsKey(String clave){
+		return false;
+		
+	}
+
+	//borra el par asociado en caso de realizarse  con exito devuelve true o false si no
+	public boolean remove(String clave){
+		return false;
+		
+	}
+	
+}
---
+++ ArrayAsociativoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArrayAsociativoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
