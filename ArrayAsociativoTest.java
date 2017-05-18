54db10481f736a63d7c82d6238cb9d7c0bf6c381
JorgeL <jorgepalomares42@hotmail.com>
2017-05-18 18:42:30 +0200

    AS.2Fallo

--- ArrayAsociativoTest.java
+++ ArrayAsociativoTest.java
@@ -1,18 +1,37 @@
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ArrayAsociativoTest {
 	private ArrayAsociativo a;
 	
-	
+	private String[] claves = {"nombre","apellido","dni","pais"};
+	private String[] valores = {"luis","martin","12345567P","Espana"};
+
+	@Before
+	public void inicializar(){
+	a= new ArrayAsociativo(claves,valores);
+	System.out.println("Before");
+	}
+	@After 
+	public void finalizar(){
+	a=null;
+	System.out.println("After");
+	}
+
 	
 	
 	@Test
 	public void ArrayVacio() {
 		a= new ArrayAsociativo();
 		assertTrue(a.size()==0);
-		a=null;
+	}
+	
+	@Test
+	public void ArrayNoVacio() {
+		assertTrue(a.size()==4);
 	}
 
 }
